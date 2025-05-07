20f08cd47e71c2a6408677dd31a8a23d6599afd4
abhinav1-sjs <vaishrajg58@gmail.com>
2025-05-07 16:59:03 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-// Example: Toggle mobile navigation
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelector('.nav-links');
-
-navToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('nav-links_visible');
-});
